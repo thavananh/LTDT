9bbf5ac0bb735dc2302935fe7ea6fb8ea56d0d2a
DuySieuDepTrai <4801104035@student.hcmue.edu.vn>
2023-12-11 01:31:18 +0700

    Add project files.

---
+++ LTDT/GradientLabel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LTDT
+{
+    class GradientLabel : Label
+    {
+        private Color backgroundColorTop;
+        private Color backgroundColorBottom;
+        private Color textColorTop = Color.Black;
+        private Color textColorBottom = Color.Black;
+        private float backgroundGradientAngel;
+        private float textGradientAngel;
+        private StringAlignment aligntment = StringAlignment.Center;
+        private StringAlignment lineAlignment = StringAlignment.Center;
+
+        public Color BackgroundColorTop { get => backgroundColorTop; set => backgroundColorTop = value; }
+        public Color BackgroundColorBottom { get => backgroundColorBottom; set => backgroundColorBottom = value; }
+        public Color TextColorTop { get => textColorTop; set => textColorTop = value; }
+        public Color TextColorBottom { get => textColorBottom; set => textColorBottom = value; }
+        public float BackgroundGradientAngel { get => backgroundGradientAngel; set => backgroundGradientAngel = value; }
+        public float TextGradientAngel { get => textGradientAngel; set => textGradientAngel = value; }
+        public StringAlignment Aligntment { get => aligntment; set => aligntment = value; }
+        public StringAlignment LineAlignment { get => lineAlignment; set => lineAlignment = value; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            LinearGradientBrush backgroundGradientBrush = new LinearGradientBrush(this.ClientRectangle, this.BackgroundColorTop, this.BackgroundColorBottom, this.BackgroundGradientAngel);
+            Graphics graphic = e.Graphics;
+            graphic.FillRectangle(backgroundGradientBrush, this.ClientRectangle);
+
+            Rectangle rect = new Rectangle(0 - 5, 0, this.Width + 10, this.Height);
+            LinearGradientBrush textGradientBrush = new LinearGradientBrush(this.ClientRectangle, this.TextColorTop, this.TextColorBottom, this.TextGradientAngel);
+            StringFormat stringFormat = new StringFormat()
+            {
+                LineAlignment = Aligntment,
+                Alignment = LineAlignment,
+                FormatFlags = StringFormatFlags.NoWrap,
+                //Trimming = StringTrimming.EllipsisCharacter
+            };
+            e.Graphics.DrawString(this.Text, this.Font, textGradientBrush, rect, stringFormat);
+        }
+    }
+}
---
+++ LTDT/GradientPictureBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LTDT
+{
+    class GradientPictureBox : PictureBox
+    {
+        private Color colorTop;
+        private Color colorBottom;
+        private float gradientAngle = 90f;
+        public Color ColorTop { get => colorTop; set => colorTop = value; }
+        public Color ColorBottom { get => colorBottom; set => colorBottom = value; }
+        public float GradientAngle { get => gradientAngle; set => gradientAngle = value; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            LinearGradientBrush linearGradientBrush = new LinearGradientBrush(this.ClientRectangle, this.ColorTop, this.ColorBottom, this.GradientAngle);
+            Graphics graphic = e.Graphics;
+            graphic.FillRectangle(linearGradientBrush, this.ClientRectangle);
+            base.OnPaint(e);
+        }
+    }
+}
